543c86a1bf0ae74440038573c8ca07afc8b8972d
Lu <sunlufinal@hotmail.com>
2016-01-31 11:28:32 -0500

    Added Java/BufferOrientedFileIO.java

---
+++ Java/BufferOrientedFileIO.java
@@ -0,0 +1,29 @@
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.io.IOException;
+
+public class BufferOrientedFileIO {
+  public static void main(String[] args) {
+    Path inputFilepath = Paths.get(".", "in.txt");
+    Path outputFilepath = Paths.get(".", "out.txt");
+    Charset charset = Charset.forName("UTF-8");
+    try {
+      BufferedReader reader = Files.newBufferedReader(inputFilepath, charset);
+      BufferedWriter writer = Files.newBufferedWriter(outputFilepath, charset);
+      String line;
+      while ((line = reader.readLine()) != null) {
+        line += '\n';
+        writer.write(line, 0, line.length());
+      }
+      reader.close();
+      writer.close();
+    } catch (IOException e) {
+      System.err.format("IOException: %s%n", e);
+    }
+  }
+}
